ee6b156aaa47cccd90f4f78f2be1b210fc33f87b
Shekhar Gulati <shekhargulati84@gmail.com>
2013-08-27 11:49:33 +0530

    added base template and application layout

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 from app import views , models
--- app/views.py
+++ app/views.py
@@ -1,14 +1,24 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-	return '''
-		<html>
-			<head>
-				<title>Welcome to LocalJobs</title>
-			</head>
-			<body>
-				<h1>Hello Folks !! Welcome to LocalJobs</h1>
-			</body>
-		</html>
-	'''
+	return render_template('index.html',title="Welcome to LocalJobs -- Location Aware Job Search Application")
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html',title="About LocalJobs")
+
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html' , title="Contact Us")
+
+@app.route('/signin')
+def signin():
+	return render_template('signin.html' , title="Signin to LocalJobs")
+
+@app.route('/register')
+def register():
+	return render_template('register.html' , title="Register for LocalJobs Account")
