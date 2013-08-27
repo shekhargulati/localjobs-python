51ee503b35b9904c4b1ac63d8d178cc01d401c7c
Shekhar Gulati <shekhargulati84@gmail.com>
2013-08-27 15:11:24 +0530

    registeration and signin functionality working now

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import pymongo
 from pymongo import MongoClient
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -8,5 +9,11 @@ app.config['SECRET_KEY'] = 'secret_key'
 
 client = MongoClient('localhost', 27017)
 db = client['localjobs']
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = 'signin'
+
 
 from app import views , models
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
-from app import app , db
-from flask import render_template , request , flash , redirect , url_for
+from app import app , db , login_manager
+from flask import render_template , request , flash , redirect , url_for ,g
 from models import Users
+from flask.ext.login import login_user , logout_user , current_user , login_required
+from bson.objectid import ObjectId
 
 @app.route('/')
+@login_required
 def index():
 	return render_template('index.html',title="Welcome to LocalJobs -- Location Aware Job Search Application")
 
@@ -16,11 +19,27 @@ def about():
 def contact():
 	return render_template('contact.html' , title="Contact Us")
 
-@app.route('/signin')
+@app.route('/signin' , methods=['GET','POST'])
 def signin():
-	return render_template('signin.html' , title="Signin to LocalJobs")
+	if request.method == 'GET':
+		return render_template('signin.html' , title="Signin to LocalJobs")
 
-@app.route('/register',methods=['GET','POST'])
+	email = request.form['email']
+	password = request.form['password']
+	remember_me = False
+	if 'rememberme' in request.form:
+		remember_me = True
+	users_dict = db.users.find_one({'email':email , 'password':password})
+	if users_dict is None:
+		flash('Email or password is invalid' , 'error')
+		return redirect(url_for('signin'))
+	registered_user = Users(users_dict.get('email'),users_dict.get('password'),users_dict.get('linkedin_profile_url'),users_dict.get('skills'))
+	registered_user.id = users_dict.get('_id')
+	login_user(registered_user, remember = remember_me)
+	flash('Logged in successfully')
+	return redirect(request.args.get('next') or url_for('index'))
+
+@app.route('/register' , methods=['GET','POST'])
 def register():
 	if request.method == 'GET':
 		return render_template('register.html' , title="Register for LocalJobs Account")
@@ -32,3 +51,19 @@ def register():
 	user_id = db.users.insert(user.__dict__ , w=1)
 	flash(u'User created with id %s' % user_id)
 	return redirect(url_for('signin'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index')) 
+
+@login_manager.user_loader
+def load_user(id):
+	users_dict = db.users.find_one({"_id": ObjectId(str(id))})
+	registered_user = Users(users_dict.get('email'),users_dict.get('password'),users_dict.get('linkedin_profile_url'),users_dict.get('skills'))
+	registered_user.id = users_dict.get('_id')
+	return registered_user
+
+@app.before_request
+def before_request():
+    g.user = current_user
